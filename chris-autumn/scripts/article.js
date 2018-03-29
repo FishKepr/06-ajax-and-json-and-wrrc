457a21c4432f44d12397652cc4fd1d9b1ca41286
SeatownAC <33205876+SeatownAC@users.noreply.github.com>
2018-03-28 21:10:41 -0700

    completed local storage

--- chris-autumn/scripts/article.js
+++ chris-autumn/scripts/article.js
@@ -10,49 +10,57 @@ function Article (rawDataObj) {
   this.publishedOn = rawDataObj.publishedOn;
 }
 
-// REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
+// REVIEWED: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
-// COMMENT: Why isn't this method written as an arrow function?
-// PUT YOUR RESPONSE HERE
+// COMMENTed: Why isn't this method written as an arrow function?
+// As a general rule, you cannot use 'this' with arrow functions
 Article.prototype.toHtml = function() {
   let template = Handlebars.compile($('#article-template').text());
 
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
 
-  // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
+  // COMMENTED: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
   // Not sure? Check the docs!
-  // PUT YOUR RESPONSE HERE
+  // Checking to see if a 'published on' date exists, if so it calculates and posts the delta days, if not, it posts 'draft'
+  // The question mark is the ternary operator, the semi-colon seperates the if/else
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   this.body = marked(this.body);
 
   return template(this);
 };
 
-// REVIEW: There are some other functions that also relate to all articles across the board, rather than just single instances. Object-oriented programming would call these "class-level" functions, that are relevant to the entire "class" of objects that are Articles.
+// REVIEWED: There are some other functions that also relate to all articles across the board, rather than just single instances. Object-oriented programming would call these "class-level" functions, that are relevant to the entire "class" of objects that are Articles.
 
-// REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
+// REVIEWED: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
-// COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
-// PUT YOUR RESPONSE HERE
+// COMMENTED: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
+// This is called from the fetchAll function. 
+// rawData used to be an array, but in this lab it is the local storage entity
 Article.loadAll = articleData => {
   articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
 
   articleData.forEach(articleObject => Article.all.push(new Article(articleObject)))
 }
 
-// REVIEW: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
+// REVIEWED: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
 Article.fetchAll = () => {
-  // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
+  // REVIEWED: What is this 'if' statement checking for? Where was the rawData set to local storage?
   if (localStorage.rawData) {
     console.log('Found Local Storage rawData');
-    Article.loadAll();
+    let strRetrieveLocal = localStorage.getItem('rawData');
+    //Article.all = JSON.parse(strRetrieveLocal);
+
+    Article.loadAll(JSON.parse(strRetrieveLocal));
+    articleView.initIndexPage();
 
   } else {
     $.getJSON(url)
       .then(articles => {
         // console.log('Articles:', articles.length);
         Article.loadAll(articles);
+        let localRawData = JSON.stringify(articles);
+        localStorage.setItem('rawData', localRawData);
         articleView.initIndexPage();
         // articles.forEach(article => {
         //   // console.log(article);
